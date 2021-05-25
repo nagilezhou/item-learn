f90745489173c25f5e345f92ac0b2d93f34716c7
莫须 <nagile.zt@alibaba-inc.com>
2021-05-25 22:26:47 +0800

    zt保存刷题进度

--- src/main/java/leetcode/editor/cn/P763PartitionLabels.java
+++ src/main/java/leetcode/editor/cn/P763PartitionLabels.java
@@ -93,6 +93,23 @@ public class P763PartitionLabels {
             return res2;
         }
     }
+    public List<Integer> partitionLabels2(String s) {
+        int[] last = new int[26];
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            last[s.charAt(i) - 'a'] = i;
+        }
+        List<Integer> partition = new ArrayList<Integer>();
+        int start = 0, end = 0;
+        for (int i = 0; i < length; i++) {
+            end = Math.max(end, last[s.charAt(i) - 'a']);
+            if (i == end) {
+                partition.add(end - start + 1);
+                start = end + 1;
+            }
+        }
+        return partition;
+    }
     //leetcode submit region end(Prohibit modification and deletion)
 
 }
