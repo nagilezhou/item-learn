c57fbe2abb66694393f9b3862b0fac3f6a7475a5
莫须 <nagile.zt@alibaba-inc.com>
2021-07-07 23:35:53 +0800

    zt保存刷题进度

---
+++ src/main/java/leetcode/editor/cn/P1301NumberOfPathsWithMaxScore.java
@@ -0,0 +1,115 @@
+//You are given a square board of characters. You can move on the board starting
+// at the bottom right square marked with the character 'S'.
+//
+// You need to reach the top left square marked with the character 'E'. The rest
+// of the squares are labeled either with a numeric character 1, 2, ..., 9 or with
+// an obstacle 'X'. In one move you can go up, left or up-left (diagonally) only i
+//f there is no obstacle there.
+//
+// Return a list of two integers: the first integer is the maximum sum of numeri
+//c characters you can collect, and the second is the number of such paths that yo
+//u can take to get that maximum sum, taken modulo 10^9 + 7.
+//
+// In case there is no path, return [0, 0].
+//
+//
+// Example 1:
+// Input: board = ["E23","2X2","12S"]
+//Output: [7,1]
+// Example 2:
+// Input: board = ["E12","1X1","21S"]
+//Output: [4,2]
+// Example 3:
+// Input: board = ["E11","XXX","11S"]
+//Output: [0,0]
+//
+//
+// Constraints:
+//
+//
+// 2 <= board.length == board[i].length <= 100
+// Related Topics 数组 动态规划 矩阵
+// 👍 33 👎 0
+
+package leetcode.editor.cn;
+
+import java.util.Arrays;
+import java.util.List;
+
+//Java：Number of Paths with Max Score
+public class P1301NumberOfPathsWithMaxScore {
+    public static void main(String[] args) {
+        Solution solution = new P1301NumberOfPathsWithMaxScore().new Solution();
+        System.out.println(Arrays.toString(solution.pathsWithMaxScore(Arrays.asList("E11","XXX","11S"))));
+        // TO TEST
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int[] pathsWithMaxScore(List<String> board) {
+            int mod = 1000000007;
+            int n = board.size();
+            int m = board.get(0).length();
+            int[][][] dp = new int[n][m][2];
+
+            dp[n - 1][m - 1][1] = 1;
+            for (int i = n - 1; i >= 0; i--) {
+                for (int j = m - 1; j >= 0; j--) {
+                    char c = board.get(i).charAt(j);
+                    if (c == 'S' || c == 'X') {
+                        continue;
+                    }
+
+                    int maxScore = -1;
+                    int path = 0;
+                    if (i + 1 < n) {
+                        if (dp[i + 1][j][1] != -0) {
+                            if (dp[i + 1][j][0] == maxScore) {
+                                path += dp[i + 1][j][1];
+                            } else if (dp[i + 1][j][0] > maxScore) {
+                                maxScore = dp[i + 1][j][0];
+                                path = dp[i + 1][j][1];
+                            }
+                        }
+                    }
+                    if (j + 1 < m) {
+                        if (dp[i][j + 1][1] != 0) {
+                            if (dp[i][j + 1][0] == maxScore) {
+                                path += dp[i][j + 1][1];
+                            } else if (dp[i][j + 1][0] > maxScore) {
+                                maxScore = dp[i][j + 1][0];
+                                path = dp[i][j + 1][1];
+                            }
+                        }
+                    }
+                    if (i + 1 < n && j + 1 < m) {
+                        if (dp[i + 1][j + 1][1] != 0) {
+                            if (dp[i + 1][j + 1][0] == maxScore) {
+                                path += dp[i + 1][j + 1][1];
+                            } else if (dp[i + 1][j + 1][0] > maxScore) {
+                                maxScore = dp[i + 1][j + 1][0];
+                                path = dp[i + 1][j + 1][1];
+                            }
+                        }
+                    }
+
+                    if (path != 0) {
+                        int num = c == 'E' ? 0 : c - '0';
+                        dp[i][j][0] = maxScore + num;
+                        dp[i][j][1] = path;
+                        dp[i][j][0] %= mod;
+                        dp[i][j][1] %= mod;
+                    }
+
+                }
+            }
+            return dp[0][0];
+        }
+
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
+
+
+
---
+++ src/main/java/leetcode/editor/cn/P576OutOfBoundaryPaths.java
@@ -0,0 +1,77 @@
+//There is an m x n grid with a ball. The ball is initially at the position [sta
+//rtRow, startColumn]. You are allowed to move the ball to one of the four adjacen
+//t cells in the grid (possibly out of the grid crossing the grid boundary). You c
+//an apply at most maxMove moves to the ball.
+//
+// Given the five integers m, n, maxMove, startRow, startColumn, return the numb
+//er of paths to move the ball out of the grid boundary. Since the answer can be v
+//ery large, return it modulo 109 + 7.
+//
+//
+// Example 1:
+//
+//
+//Input: m = 2, n = 2, maxMove = 2, startRow = 0, startColumn = 0
+//Output: 6
+//
+//
+// Example 2:
+//
+//
+//Input: m = 1, n = 3, maxMove = 3, startRow = 0, startColumn = 1
+//Output: 12
+//
+//
+//
+// Constraints:
+//
+//
+// 1 <= m, n <= 50
+// 0 <= maxMove <= 50
+// 0 <= startRow < m
+// 0 <= startColumn < n
+// Related Topics 动态规划
+// 👍 127 👎 0
+
+package leetcode.editor.cn;
+
+//Java：Out of Boundary Paths
+public class P576OutOfBoundaryPaths {
+    public static void main(String[] args) {
+        Solution solution = new P576OutOfBoundaryPaths().new Solution();
+        System.out.println(solution.findPaths(1, 3, 3, 0, 1));
+        // TO TEST
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+            int mod = 1000000007;
+            int[] di = {1, -1, 0, 0};
+            int[] dj = {0, 0, 1, -1};
+            int[][][] dp = new int[maxMove + 1][m][n];
+            for (int move = 1; move <= maxMove; move++) {
+                for (int i = 0; i < m; i++) {
+                    for (int j = 0; j < n; j++) {
+                        for (int z = 0; z < di.length; z++) {
+                            int row = i + di[z];
+                            int col = j + dj[z];
+                            if (row < 0 || row >= m || col < 0 || col >= n) {
+                                dp[move][i][j] += 1;
+                                continue;
+                            }
+                            dp[move][i][j] += dp[move - 1][row][col];
+                            dp[move][i][j] %= mod;
+                        }
+                    }
+                }
+            }
+            return dp[maxMove][startRow][startColumn];
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
+
+
+
