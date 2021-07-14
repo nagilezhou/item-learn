e4aafe09f2bb084846c0069a4e017f83f86d9877
莫须 <nagile.zt@alibaba-inc.com>
2021-07-14 12:53:59 +0800

    zt保存刷题进度

---
+++ src/main/java/leetcode/editor/cn/P118PascalsTriangle.java
@@ -0,0 +1,59 @@
+//Given an integer numRows, return the first numRows of Pascal's triangle.
+//
+// In Pascal's triangle, each number is the sum of the two numbers directly abov
+//e it as shown:
+//
+//
+// Example 1:
+// Input: numRows = 5
+//Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+// Example 2:
+// Input: numRows = 1
+//Output: [[1]]
+//
+//
+// Constraints:
+//
+//
+// 1 <= numRows <= 30
+//
+// Related Topics 数组 动态规划
+// 👍 527 👎 0
+
+package leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//Java：Pascal's Triangle
+public class P118PascalsTriangle{
+    public static void main(String[] args) {
+        Solution solution = new P118PascalsTriangle().new Solution();
+        System.out.println(solution.generate(5));
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i = 0; i < numRows; i++){
+            List<Integer> list = new ArrayList<>();
+            for (int j = 0; j <= i; j++){
+                if(j == 0 || j == i){
+                    list.add(1);
+                    continue;
+                }
+                list.add(res.get(i - 1).get(j - 1) + res.get(i - 1).get(j));
+            }
+            res.add(list);
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+
+
+
